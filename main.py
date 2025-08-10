6f472ac4cf58fc423584596c9761337e7e8a00e1
Vadim <vblinov2009.rus@yandex.ru>
2025-08-10 06:07:40 +0300

    Внесение сумм в БД

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
+from database import Session, Prices
+
+def get_total_price(session):
+    sums = [i.price for i in session.query(Prices).all()]
+    return sum(sums)
+
 if __name__ == '__main__':
-    print("Выберите действие")
+    session = Session()
+    print("Выберите действие.", end=" ")
     while True:
         action = input("(1) - вписать новую сумму (2) - просмотреть траты: ")
 
         if action == "1":
+            road_price = input("Введите сумму: ")
+            while True:
+                if not road_price.isdigit():
+                    road_price = input("Это не сумма! Попробуйте еще раз: ")
+                else:
+                    data = Prices(price=road_price)
+                    session.add(data)
+                    session.commit()
+                    print(f"Итоговая сумма: {get_total_price(session)}")
+                    break
             break
 
         elif action == "2":
-            break
+            pass
 
         else:
-            if action.isdigit(): print("Нет такого варианта!")
-            else: print("Впишите номер!")
+            if action.isdigit(): print("Нет такого варианта!", end=" ")
+            else: print("Впишите номер!", end=" ")
